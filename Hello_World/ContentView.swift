3ed3b47e532681c1cb00212573cf10190bf6a171
formidablae <81068781+formidablae@users.noreply.github.com>
2021-11-07 00:39:54 +0100

    Creating project for iOS app

---
+++ Hello_World/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  Hello_World
+//
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Hello_World/Hello_WorldApp.swift
@@ -0,0 +1,16 @@
+//
+//  Hello_WorldApp.swift
+//  Hello_World
+//
+//
+
+import SwiftUI
+
+@main
+struct Hello_WorldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
